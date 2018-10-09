e5083b1463b82bdeb1bbf109db7e0dc08369c8c0
zyy120 <1014868589@qq.com>
2018-10-09 18:33:13 +0800

    db

---
+++ chatroom-master/src/main/resources/db/chatr-db.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `t_user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
